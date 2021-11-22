f60162dd5eef163c3159ab6d336f3ba6b6dd8dbc
nick dvt <doanvantai0209@gmail.com>
2021-11-22 11:11:28 +0700

    fix menu

---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuOpen = document.querySelector(".header__toggle");
+    const menuClose = document.querySelector(".menu-close");
+    const menu = document.querySelector(".menu");
+    if (!menuOpen || !menuClose || !menu) return;
+    menuOpen.addEventListener("click", function () {
+        console.log("oke")
+        menu.classList.add("active")
+    });
+    menuClose.addEventListener("click", function () {
+        menu.classList.remove("active")
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelectorAll('header.header');
+    var header = header[0];
+    window.addEventListener("scroll", function () {
+        var x = pageYOffset;
+        if (x > 100) {
+            header.classList.add('header__fixed');
+            document.getElementById("rocket").style.display = 'block';
+        }
+        else {
+            header.classList.remove('header__fixed');
+            document.getElementById("rocket").style.display = 'none';
+        }
+
+    })
+})
